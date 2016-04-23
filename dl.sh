9a7cffc5a928d66f15f780a47337e516801d70aa
Charles Eckman <cceckman@gmail.com>
2016-04-23 13:08:51 -0700

    Create dl.sh

---
+++ dl.sh
@@ -0,0 +1,2 @@
+# Run: bash --init-file <(curl -L -o- cceckman.com/distro/dl.sh)
+curl -o /tmp/arch-vm.sh https://raw.githubusercontent.com/cceckman/distro/master/arch-install.sh && chmod +x /tmp/arch-install.sh && /tmp/arch-install.sh base-install
