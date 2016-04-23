4334bfa67383440e8b9fb4b3d6f460c1ed79a72e
Charles Eckman <cceckman@gmail.com>
2016-04-23 13:15:57 -0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,2 +1,2 @@
 # Run: bash --init-file <(curl -L -o- cceckman.com/distro/dl.sh)
-curl -o /tmp/arch-vm.sh https://raw.githubusercontent.com/cceckman/distro/master/arch-vm.sh && chmod +x /tmp/arch-vml.sh && /tmp/arch-vm.sh base-install
+curl -o /tmp/arch-vm.sh https://raw.githubusercontent.com/cceckman/distro/master/arch-vm.sh && chmod +x /tmp/arch-vm.sh && /tmp/arch-vm.sh base-install
