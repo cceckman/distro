28b8c30344e2f66c8520548140d042bc5ca84ebe
Charles Eckman <charles@cceckman.com>
2016-06-19 19:24:03 +0000

    Tweak virtualbox setup; use systemd for firstboot.

--- platforms/vbox/image.chroot.sh
+++ platforms/vbox/image.chroot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -i
 # Set up the chroot of the new VM.
 
+f=common/err.fn.sh; source $f || { >&2 echo "$f not found!" && exit 2; }
+f=common/mkfirstboot.fn.sh; source $f || { >&2 echo "$f not found!" && exit 2; }
+
 set -e
 for sig in INT TERM EXIT; do
   trap "echo 'Encountered an error! Dropping into bash. && bash; [[ $sig == EXIT ]] || (trap - $sig EXIT; kill -$sig $$)" $sig 
@@ -31,3 +34,18 @@ systemctl enable dhcpcd@$(ip link | grep -Po '(en|eth)[^: ]*(?=:)').service
 
 
 # TODO Auto-start the firstboot upon reboot.
+servicepath=/etc/systemd/system/
+binpath=/usr/bin/firstboot.sh
+mkdir $servicepath
+cat <<EOF >$servicepath/ambroix.service
+[Unit]
+Description=Set up Ambroix install (http://cceckman.com/r/distro)
+Requires=network-online.target
+
+[Service]
+Type=oneshot
+ExecStart=$binpath
+
+EOF
+mkfirstboot > $binpath
+chmod +x $binpath
