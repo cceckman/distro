7b5b7fec705341d47abec198c41d455c93a68c4b
Charles Eckman <charles@cceckman.com>
2016-06-22 07:25:43 +0000

    Add draft setup for RPi, cloned from GCE.

---
+++ platforms/rpi3/image.root.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# run-as-root portion of setting up image.
+
+# TODO trap
+set -e
+
+# Download in background
+mkdir /tmp/download
+curl -Lo /tmp/download/arch-rpi.tar.gz \
+  http://archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz &
+
+disk="$1"
+hostname="$2"
+
+# Per https://archlinuxarm.org/platforms/armv8/broadcom/raspberry-pi-3
+fdisk "$disk" <<EOF
+o
+p
+n
+p
+1
+
++100M
+t
+c
+n
+p
+2
+
+
+w
+EOF
+
+
+mkdir -p /mnt/boot
+mkdir -p /mnt/root
+mkfs.vfat "${disk}1" && mount "${disk}1" /mnt/boot
+mkfs.ext4 "${disk}2" && mount  "${disk}2" /mnt/root
+
+wait
+# Wait for download to complete
+bsdtar -xpf /tmp/download/arch-rpi.tar.gz -C /mnt/root
+mv /mnt/root/boot/* /mnt/boot
+sync
