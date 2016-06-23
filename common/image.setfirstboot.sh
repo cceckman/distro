7f943b1e1b22c08876b59d1dbe0a8ecc4dcc9033
Charles Eckman <charles@cceckman.com>
2016-06-23 04:50:23 +0000

    Fix service install paths in setfirstboot.
    This installed on the host, not the remote. Whomp.

--- common/image.setfirstboot.sh
+++ common/image.setfirstboot.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-# Call against the root directory of an image
+# Call against the root directory of an image to set up the first-boot script.
 
 f=common/mkfirstboot.fn.sh; source $f || { >&2 echo "$f not found!" && exit 2; }
 
-pushd $1
+MOUNT="$1"
 
-servicepath=/etc/systemd/system
 binpath=/usr/bin/firstboot.sh
-mkdir $servicepath
-cat <<EOF >$servicepath/ambroix.service
+mkdir "${MOUNT}${servicepath}"
+cat <<EOF >${MOUNT}/etc/systemd/system/ambroix.service
 [Unit]
 Description=Set up Ambroix install (http://cceckman.com/r/distro)
-Requires=network-online.target
+Requires=network.target
 [Service]
 Type=oneshot
 ExecStart=$binpath
 EOF
-mkfirstboot > $binpath
-chmod +x $binpath
+mkfirstboot > "${MOUNT}${binpath}"
+chmod +x "${MOUNT}${binpath}"
 
-popd
+cat <<EOF >${MOUNT}/etc/systemd/system-preset/50-ambroix.service
+enable ambroix.service
+disable *
