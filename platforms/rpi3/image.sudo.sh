9b308ccbf8206d489db2464180f93448833db903
Charles Eckman <charles@cceckman.com>
2016-06-24 05:49:49 +0000

    Sync earlier, per wiki advice.
    Not sure if this would help vs. the consistency issues I'm seeing, but
    here's hoping.

--- platforms/rpi3/image.sudo.sh
+++ platforms/rpi3/image.sudo.sh
@@ -34,29 +34,48 @@ p
 w
 EOF
 
+if [ "$?" != "0" ]
+then
+ echo "Could not format disk" && exit 2
+fi
+
 # TODO use different directories here.
 mkdir -p /mnt/boot
 mkdir -p /mnt/root
-mkfs.vfat "${disk}1" && mount "${disk}1" /mnt/boot
-mkfs.ext4 "${disk}2" && mount  "${disk}2" /mnt/root
+mkfs.vfat "${disk}1" && mount "${disk}1" /mnt/boot || {
+  echo "Could not format boot partition" && exit 3
+}
+mkfs.ext4 "${disk}2" && mount  "${disk}2" /mnt/root || {
+  echo "Could not format root partition" && exit 4
+}
 
 echo "Waiting for image download to complete..."
 wait
 echo "Done!"
 
 echo "Copying image to /mnt/root..."
-bsdtar -xpf $IMGPATH -C /mnt/root
+bsdtar -xpf $IMGPATH -C /mnt/root || {
+  echo "Failed to populate root!" && exit 5
+}
+echo "Done!"
+
+echo "Syncing filesystems..."
+sync || { echo "Failed to sync!" && exit 5; }
 echo "Done!"
 
 # TODO install qemu, etc., run some commands within chroot.
 # https://wiki.archlinux.org/index.php/Raspberry_Pi#QEMU_chroot
 
 echo "Setting up first-boot behavior..."
-common/image.setfirstboot.sh /mnt/root
+common/image.setfirstboot.sh /mnt/root || {
+  echo "Could not set up boot behavior!" && exit 6
+}
 echo "Done!"
 
 echo "Copying to /mnt/boot..."
-mv /mnt/root/boot/* /mnt/boot
+mv /mnt/root/boot/* /mnt/boot || {
+  echo "Could not populate boot partition!" && exit 7
+}
 echo "Done!"
 
 echo "Syncing filesystem to media..."
@@ -64,12 +83,17 @@ sync
 echo "Done!"
 
 echo "Unmounting..."
-umount /mnt/boot
-umount /mnt/root
+{
+  umount /mnt/boot && umount /mnt/root
+} || {
+  echo "Unmount failed!" && exit 8
+}
 echo "Done!"
 
 echo "Checking filesystems..."
-{ fsck "${disk}1" && fsck "${disk}2" } || {
+{
+  fsck "${disk}1" && fsck "${disk}2"
+} || {
   echo "Filesystem check failed!"
   exit 9
 }
