22c6eba7889e3103836418a3b932b6f7063c8b3a
Charles Eckman <charles@cceckman.com>
2016-08-26 05:05:59 +0000

    Move some package installs into the image step.

--- platforms/vbox/image.chroot.sh
+++ platforms/vbox/image.chroot.sh
@@ -9,18 +9,13 @@ done
 set -x
 
 # Set up VirtualBox.
-pacman-key --refresh-keys
-pacman --noconfirm -Sy linux-headers virtualbox-guest-utils virtualbox-guest-dkms linux-headers
 systemctl enable vboxservice.service
-
 
 vbox_modules="vboxguest vboxsf vboxvideo"
 sed -i "s/^MODULES=\"/MODULES=\"$vbox_modules/" /etc/mkinitcpio.conf
 
 mkinitcpio -p linux
 
-# Install GRUB
-pacman --noconfirm -Sy grub
 grub-install --target=i386-pc --recheck /dev/sda
 
 echo 'GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT splash iomem=relaxed"' >> /etc/default/grub
@@ -28,8 +23,5 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 # Ensure that dhcpcd starts next time around
 systemctl enable dhcpcd.service
-# And that we can tell the network's up
-pacman --noconfirm -Sy fping
 
-# TODO Auto-start the firstboot upon reboot.
 trap - EXIT
